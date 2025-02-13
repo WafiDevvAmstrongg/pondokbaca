5b136e54e96fa89e50e2d7426747cdacc9588c3b
henryjonathnn <henryjonathanchandra12@gmail.com>
2025-02-13 22:15:56 +0700

    add login and register logic and set the endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('app');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', function () {
+        return view('livewire.auth.login');
+    })->name('login');
+
+    Route::get('/register', function () {
+        return view('livewire.auth.register');
+    })->name('register');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    
+    Route::post('/logout', function () {
+        Auth::logout();
+        session()->invalidate();
+        session()->regenerateToken();
+        return redirect('/');
+    })->name('logout');
 });
