947eb963fd1f3edf5c1267ce1d43a09d0d967504
henryjonathnn <henryjonathanchandra12@gmail.com>
2025-02-13 13:32:03 +0700

    setup project laravel livewire alpinejs tailwindcss

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,4 @@
+<div class="text-center">
+    <h1 class="text-2xl font-bold">Counter: {{ $count }}</h1>
+    <button wire:click="increment" class="bg-blue-500 text-white px-4 py-2 rounded">Tambah</button>
+</div>
