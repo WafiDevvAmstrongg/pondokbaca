e431d06868a373c22c7cd4d11a51ab1f86d6357b
Henry Jonathan <henryjonathanchandra12@gmail.com>
2025-03-17 08:29:25 +0700

    tes

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -3,13 +3,8 @@
         @livewire('layouts.navigation')
         @livewire('auth.auth-modals')
         <div class="flex flex-1">
-            <!-- Desktop Sidebar -->
-            <div class="hidden lg:block">
-                @livewire('layouts.sidebar')
-            </div>
-
-            <!-- Main Content -->
-            <main class="flex-1 lg:ml-72 p-4 sm:p-6 lg:p-8">
+            @livewire('layouts.sidebar')
+            <main class="flex-1 ml-72 p-8">
                 {{ $slot }}
             </main>
         </div>
